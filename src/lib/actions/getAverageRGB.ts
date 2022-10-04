0ac6daf0f9b2bdc817a38576b0d98eb0e5514735
Joe Innes <joe@joeinn.es>
2022-10-04 13:38:28 +0200

    Improve bg colour sampling

--- src/lib/actions/getAverageRGB.ts
+++ src/lib/actions/getAverageRGB.ts
@@ -3,6 +3,13 @@ import type { Writable } from 'svelte/store';
 
 export const getAverageRGB = (imgEl: HTMLImageElement, storeToUpdate: Writable<string>) => {
 	imgEl.crossOrigin = 'anonymous';
-	const fac = new FastAverageColor();
-	fac.getColorAsync(imgEl).then(colour => storeToUpdate.set(colour.rgba));
+	imgEl.onload = () => {
+		const height = imgEl.naturalHeight;
+		const width = imgEl.naturalWidth;
+		const canvas = new OffscreenCanvas(width || 1024, height || 768);
+		const ctx = canvas.getContext('2d');
+		ctx?.drawImage(imgEl, 0, 0);
+		const fac = new FastAverageColor();
+		fac.getColorAsync(canvas).then(colour => storeToUpdate.set(colour.rgba));
+	}
 };
