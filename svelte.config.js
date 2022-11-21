a76cca0bfa70eddf54e68a1d0e983341d7086f31
Joe Innes <joe@joeinn.es>
2022-11-21 15:25:08 +0100

    Use auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { mdsvex } from 'mdsvex';
 import rehypeExternalLinks from 'rehype-external-links';
 import fs from 'fs';
